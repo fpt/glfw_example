32e4abc801c0439cb4253f5a381fecb20fdaaa17
youichi <yofujimo@gmail.com>
2017-01-04 15:20:52 +0900

    mouse/key

--- main.cpp
+++ main.cpp
@@ -36,9 +36,14 @@ int counter = 0;
 vec3 position = vec3( 0, 0, 5 );
 float horizontalAngle = 3.14f;
 float verticalAngle = 0.0f;
-float initialFoV = 45.0f;
-float speed = 3.0f; // 3 units / second
+float FoV = glm::radians(45.0f);
+float speed = .1f; // 3 units / second
 float mouseSpeed = 0.005f;
+bool g_rotating_flag = false;
+double lastTime = glfwGetTime();
+double g_prev_mouse_x;
+double g_prev_mouse_y;
+
 
 
 void init(void)
@@ -73,7 +78,7 @@ void fn_fbresize(GLFWwindow *window, int width, int height)
 {
     //std::cout << "fbresize" << std::endl;
     // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) width / (float)height, 0.1f, 100.0f);
+    /*glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) width / (float)height, 0.1f, 100.0f);
       
     // Or, for an ortho camera :
     //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
@@ -83,6 +88,30 @@ void fn_fbresize(GLFWwindow *window, int width, int height)
         glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
         glm::vec3(0,0,0), // and looks at the origin
         glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );*/
+
+    // Direction : Spherical coordinates to Cartesian coordinates conversion
+    glm::vec3 direction(
+        cos(verticalAngle) * sin(horizontalAngle),
+        sin(verticalAngle),
+        cos(verticalAngle) * cos(horizontalAngle)
+    );
+    // Right vector
+    glm::vec3 right = glm::vec3(
+        sin(horizontalAngle - 3.14f/2.0f),
+        0,
+        cos(horizontalAngle - 3.14f/2.0f)
+    );
+    // Up vector : perpendicular to both direction and right
+    glm::vec3 up = glm::cross( right, direction );
+
+    // Projection matrix : 45&deg; Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    mat4 Projection = glm::perspective(FoV, (float)width / (float)height, 0.1f, 100.0f);
+    // Camera matrix
+    mat4 View = glm::lookAt(
+        position,           // Camera is here
+        position + direction, // and looks here : at the same position, plus "direction"
+        up                  // Head is up (set to 0,-1,0 to look upside-down)
     );
 
     // Model matrix : an identity matrix (model will be at the origin)
@@ -143,10 +172,69 @@ void fn_refresh(GLFWwindow* window)
     //glFlush();
 }
 
+void fn_cursorpos(GLFWwindow *window, double x, double y) {
+    if (g_rotating_flag) {
+        // Compute new orientation
+        horizontalAngle += mouseSpeed * float( x - g_prev_mouse_x );
+        verticalAngle   += mouseSpeed * float( y - g_prev_mouse_y );
+    }
+
+    g_prev_mouse_x = x;
+    g_prev_mouse_y = y;
+}
+
+void fn_mousebtn(GLFWwindow *window, int btn, int act, int mod) {
+    if (btn == GLFW_MOUSE_BUTTON_1 && act == GLFW_PRESS) {
+        g_rotating_flag = true;
+        // setcapture or glfwSetInputMode and GLFW_CURSOR_DISABLED?
+    } else {
+        // GLFW_RELEASE
+        // releasecapture
+        g_rotating_flag = false;
+    }
+}
+
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    if (action == GLFW_PRESS) {
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+
+
+        // Direction : Spherical coordinates to Cartesian coordinates conversion
+        glm::vec3 direction(
+            cos(verticalAngle) * sin(horizontalAngle),
+            sin(verticalAngle),
+            cos(verticalAngle) * cos(horizontalAngle)
+        );
+        // Right vector
+        glm::vec3 right = glm::vec3(
+            sin(horizontalAngle - 3.14f/2.0f),
+            0,
+            cos(horizontalAngle - 3.14f/2.0f)
+        );
+
+        // Move forward
+        if (key == GLFW_KEY_UP && action == GLFW_PRESS){
+            position += direction * speed;
+            std::cout << "U" << std::endl;
+        }
+        // Move backward
+        if (key == GLFW_KEY_DOWN && action == GLFW_PRESS){
+            position -= direction * speed;
+            std::cout << "D" << std::endl;
+        }
+        // Strafe right
+        if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS){
+            position += right * speed;
+            std::cout << "R" << std::endl;
+        }
+        // Strafe left
+        if (key == GLFW_KEY_LEFT && action == GLFW_PRESS){
+            position -= right * speed;
+            std::cout << "L" << std::endl;
+        }
     }
 }
 
@@ -185,6 +273,8 @@ int main(int argc, char *argv[])
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     glfwSetWindowRefreshCallback(window, fn_refresh);
     glfwSetFramebufferSizeCallback(window, fn_fbresize);
+    glfwSetMouseButtonCallback(window, fn_mousebtn);
+    glfwSetCursorPosCallback(window, fn_cursorpos);
     glfwSetKeyCallback(window, key_callback);
     init();
 
